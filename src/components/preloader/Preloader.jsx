f293d1d38def0e08b84e4fa3fd223d1befc0dba3
artem <my@example.com>
2020-08-17 14:53:24 +0300

    preloader text

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -6,15 +6,56 @@ const useStylesPreloader = makeStyles(theme => ({
     overflowY: 'hidden !important',
     overflowX: 'hidden !important',
     minHeight: '98vh',
-    backgroundColor: 'black',
+    fontFamily: 'consolas',
+    backgroundColor: 'aquamarine',
+  },
+  wavy: {
+    position: 'relative',
+    // mirrow
+    '-webkit-box-reflect': 'below -12px linear-gradient(transparent, rgba(0,0,0,0.2))',
+    // affect children element
+    '& span': {
+      fontSize: '2rem',
+      position: 'relative',
+      display: 'inline-block',
+      color: '#fff',
+      textTransform: 'uppercase',
+      animation: '$animate 1s ease-in-out infinite'
+    }
+    // affect children styles
+    // '&.active'
+    // '&:hover'
+  },
+  '@keyframes animate': {
+    '0%': {
+      transform: 'translateY(8px)',
+    },
+    '20%': {
+      transform: 'translateY(28px)',
+    },
   }
+  
 }))
 
 const Preloader = () => {
   const classes = useStylesPreloader()
 
   return(
-    <div className={classes.root}>Preloader running ...</div>
+    <div className={classes.root}>
+      <div className={classes.wavy}>
+        <span>L</span>
+        <span>o</span>
+        <span>a</span>
+        <span>d</span>
+        <span>i</span>
+        <span>n</span>
+        <span>i</span>
+        <span>g</span>
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </div>
+    </div>
   )
 }
 
